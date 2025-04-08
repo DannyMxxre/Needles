d57026ba3949e74b3ee18b94aae63c8a0fc91d4c
Danny Mxxre <dannymxxre@gmail.com>
2025-04-09 01:23:26 +0300

    -

---
+++ src/examples/samples.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+from math import sqrt
+
+x = 10
+y = 20
+
+@staticmethod
+def foo():
+    pass
+
+def bar():
+    z = 42
+
+class MyClass:
+    def method(self):
+        pass
